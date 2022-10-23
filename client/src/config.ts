1069fda978e9e168dcc082ecb155f9c13514c151
solayunus <solayunus19@gmail.com>
2022-10-23 19:07:31 +0100

    all code

---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = 'xec5rzi9z6';
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/prod`
+
+export const authConfig = {
+  domain: 'dev-9abjmi27.us.auth0.com',
+  clientId: 'brIGIJZmbr6huEK9RABwii3PSqUQtePo',
+  callbackUrl: 'http://localhost:3000/callback'
+};
